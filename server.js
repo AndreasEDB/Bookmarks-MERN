5b6c36510132399371b74cdf16b82b1c0735cc9f
AndreasEDB <afhoejgaard@gmail.com>
2020-11-12 11:38:23 +0100

    commit

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const cors = require('cors')
+const publicPath = path.join(__dirname, '..', 'build');
 
 // mongoose.connect('mongodb://localhost:27017/links', {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.connect('mongodb+srv://andreas:CJfgksSXZw8uMdh@andreas.ccvvu.mongodb.net/links?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
@@ -10,6 +11,12 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log('Connected to db')
+});
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
 app.use(express.json())
